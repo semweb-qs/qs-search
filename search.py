3ab8ad9f32ce8ff6c6bb18572d66c621432575d7
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-13 09:20:44 +0700

    Init corpus

--- search.py
+++ search.py
@@ -5,11 +5,19 @@ from termcolor import colored, cprint
 from fuzzy_search.fuzzy_phrase_searcher import FuzzyPhraseSearcher
 from fuzzy_search.fuzzy_phrase_model import PhraseModel
 from rank_bm25 import BM25Okapi
+from retriv import SearchEngine
 
+UNIVERSITY = "university"
+CITY = "city"
+REGION = "region"
+COUNTRY = "country"
+TYPE = "type"
+YEAR = "year"
+AWARD = "award"
 
 
 def load_csv():
-  with open('semweb-data-007.csv', 'r') as f:
+  with open('semweb-data-008.csv', 'r') as f:
     csv_file = list(csv.reader(f))
   print(csv_file[0])
   return csv_file
@@ -66,27 +74,56 @@ def generate_fuzzy_model():
   res = process.extract(text1, domain_phrases, limit=5)
   print(res)
 
-def bm25_searcher():
 
+def bm25_searcher():
   csv_file = load_csv()
   head = csv_file[0]
   csv_file = csv_file[1:]
   column_size = len(csv_file[0])
   row_size = len(csv_file)
   corpus = []
+  collection = []
+  print(head[15])
+  id_exist = set()
+
+  def insert_to_collection(id, text, jenis):
+    if id in id_exist: return
+    id_exist.add(id)
+    collection.append({
+      "id": f"{id} {jenis}",
+      "text": text
+    })
 
   for i in range(row_size):
     entry = csv_file[i]
-    corpus.append(unidecode(entry[0]).lower())
+    cleaned = unidecode(entry[0]).lower()
+    corpus.append(cleaned)
+    insert_to_collection(entry[15].split('/')[-1], cleaned, UNIVERSITY)
+    try:
+      rank = entry[17].split(':')[1].replace('-', ' ')
+      insert_to_collection(entry[17].replace(':', 'R').replace('-', 'to'),
+                           f"qs world ranking peringkat ke award penghargaan {entry[1]} tahun year {rank}",
+                           AWARD)
+      insert_to_collection(entry[1],
+                           f"qs world ranking list peringkat daftar tahun year {entry[1]}",
+                           YEAR)
+    except:
+      pass
+    if(entry[9]):
+      insert_to_collection(entry[9], f"tipe type {entry[9]}" ,TYPE)
+    insert_to_collection(entry[18], f"city kota located location lokasi di at {entry[6]} {entry[5]}" ,CITY)
+    insert_to_collection(entry[19], f"nation country negara located location lokasi di at {entry[5]} {entry[7]}" ,COUNTRY)
+    insert_to_collection(entry[20], f"region benua daerah located location lokasi di at {entry[7]}" ,REGION)
+
+
+  se = SearchEngine("university")
+  print(collection[0:10])
   # print(corpus)
-  corpus = list(set(corpus))
-  corpus = [s.split() for s in corpus]
-  bm25 = BM25Okapi(corpus)
-  query = "university".split()
-  top_k = bm25.get_top_n(query, corpus, n=5)
-  print(top_k)
 
+  se.index(collection)
 
+  result = se.search("university indonesia")
+  print(result)
 
 
 def main():
